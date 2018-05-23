ca4fb9eac73ea9d7758ee13a28d99141d8c07fe4
bn245 <bn245@164.81.227.71>
2018-05-23 17:11:11 +0200

    Fin fonctionnalitée n°1 !!

--- SpaceInvaders2.0/src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders2.0/src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -3,9 +3,9 @@ import fr.unilim.iut.spaceinvaders.Vaisseau;
 
 public class SpaceInvaders {
 
-	private static final char CHAR_FIN_DE_LIGNE = '\n';
-	private static final char CHAR_VIDE = '.';
-	private static final char CHAR_VAISSEAU = 'V'; 
+	private static final char MARQUE_FIN_DE_LIGNE = '\n';
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_VAISSEAU = 'V'; 
 	private int longueur; 
 	private int hauteur;
 	Vaisseau vaisseau;
@@ -18,21 +18,48 @@ public class SpaceInvaders {
 	@Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int x = 0; x < hauteur; x++) {
-			for (int y = 0; y < longueur; y++) {
-				if (vaisseau.occupeLaPosition(x, y))
-				     espaceDeJeu.append('V');
-				else
-					espaceDeJeu.append('.');
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				char marque = recupererMarqueDeLaPosition(y, x);
+				
+			    espaceDeJeu.append(marque);
 			}
-			espaceDeJeu.append('\n');
+			espaceDeJeu.append(MARQUE_FIN_DE_LIGNE);
 		}
 		return espaceDeJeu.toString();
 	}
 
+	private char recupererMarqueDeLaPosition(int y, int x) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+		      marque=MARQUE_VAISSEAU;
+		else
+		      marque=MARQUE_VIDE;
+		return marque;
+	}
+
 	public void positionnerUnNouveauVaisseau(int x, int y) {
-		this.vaisseau = new Vaisseau(x,y);
+		
+		if (  !estDansEspaceJeu(x, y) )
+			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
+	
+		vaisseau = new Vaisseau(x, y); 
+	}
+
+	private boolean estDansEspaceJeu(int x, int y) {
+		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
 	
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return aUnVaisseauQuiOccupeLaPosition(x, y);
+	}
+
+	public void deplacerVaisseauVersLaDroite() {
+        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+	}
+	
+	public void deplacerVaisseauVersLaGauche() {
+        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaGauche();
+	}
 	
 }
